6fda1a89f743370b4f65a07ae1f140168dcae3b2
NishantPatelKsolves <nishant.patel@ksolves.com>
2024-11-27 19:50:20 +0530

    Parent-child CHECKBOX select-deselect

--- react/first-app/src/App.jsx
+++ react/first-app/src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [all, setAll] = useState(false);
+  const [one, setOne] = useState(false);
+  const [two, setTwo] = useState(false);
+  const [three, setThree] = useState(false);
+  const [four, setFour] = useState(false);
+  const [five, setFive] = useState(false);
+  
+  const handleSelectAll = (e)=>{
+    console.log('Checked:',e.target.checked)
+    setAll(e.target.checked);
+    const a = e.target.checked;
+    setOne(a);
+    setTwo(a);
+    setThree(a);
+    setFour(a);
+    setFive(a);
+    
+  }
+
+  useEffect(()=>{
+    function handleEffect(){
+      const childChecked = one && two && three && four && five;
+      setAll(childChecked);
+    }
+    handleEffect();
+  },[one, two, three, four, five])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label htmlFor="all">Select All</label>
+        <input type='checkbox' name='all' checked={all} onChange={handleSelectAll}/>
+        <br />
+        <br />
+        <label htmlFor="all">Select One</label>
+        <input type='checkbox' name='one' checked={one} onChange={(e)=>setOne(e.target.checked)}/>
+        <br />
+        <label htmlFor="all">Select Two</label>
+        <input type='checkbox' name='two' checked={two} onChange={(e)=>setTwo(e.target.checked)}/>
+        <br />
+        <label htmlFor="all">Select Three</label>
+        <input type='checkbox' name='three' checked={three} onChange={(e)=>setThree(e.target.checked)}/>
+        <br />
+        <label htmlFor="all">Select Four</label>
+        <input type='checkbox' name='four' checked={four} onChange={(e)=>setFour(e.target.checked)}/>
+        <br />
+        <label htmlFor="all">Select Five</label>
+        <input type='checkbox' name='five' checked={five} onChange={(e)=>setFive(e.target.checked)}/>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
